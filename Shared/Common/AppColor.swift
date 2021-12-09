88ad11b790df0ba58b6bb2d5bde33101a992e073
Nguyễn Đình Thạch <shikanoji@gmail.com>
2021-12-09 11:41:24 +0700

    Add AppColor

---
+++ Shared/Common/AppColor.swift
@@ -0,0 +1,19 @@
+//
+//  AppColor.swift
+//  SysVPN (iOS)
+//
+//  Created by Nguyễn Đình Thạch on 09/12/2021.
+//
+
+import Foundation
+import SwiftUI
+
+struct AppColor {
+    static var blue = Color(hex: "4142D3")
+    static var lightBlue = Color(hex: "7C7DFF")
+    static var orange = Color(hex: "FD7838")
+    static var gray = Color(hex: "E7E8F0")
+    static var darkGray = Color(hex: "E0E1EA")
+    static var green = Color(hex: "00A569")
+    static var darkNavigation = Color(hex: "363651")
+}
