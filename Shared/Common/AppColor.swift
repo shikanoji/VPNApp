da7ae531b1a19e091e822fc58d9df90ef3ad730b
Phan Văn Đa <phanvandauet1997@gmail.com>
2021-12-19 23:02:30 +0700

    Remove file

--- Shared/Common/AppColor.swift
+++ Shared/Common/AppColor.swift
@@ -14,4 +14,7 @@ struct AppColor {
     static var darkButton = Color(hex: "2E303D")
     static var background = Color(hex: "1B1C26")
     static var lightGray = Color(hex: "5B5F75")
+    static let navigationBar = Color("navigationBar")
+    static let VPNConnected = Color("VPN_connected")
+    static let VPNUnconnect = Color("VPN_unconnect")
 }
--- Shared/Common/Color+.swift
+++ Shared/Common/Color+.swift
@@ -10,7 +10,4 @@ import UIKit
 import SwiftUI
 
 extension Color {
-    static let navigationBar = Color("navigationBar")
-    static let VPNConnected = Color("VPN_connected")
-    static let VPNUnconnect = Color("VPN_unconnect")
 }
