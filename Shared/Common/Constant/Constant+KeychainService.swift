cbe8167288c70e9a69e1a73ac86f6a2d9598c262
Nguyen Dinh Thach <nguyen.dinh.thach@linecorp.com>
2022-03-07 22:50:01 +0700

    Add keychain service

---
+++ Shared/Common/Constant/Constant+KeychainService.swift
@@ -0,0 +1,13 @@
+//
+//  Constant+KeychainService.swift
+//  SysVPN (iOS)
+//
+//  Created by Nguyen Dinh Thach on 07/03/2022.
+//
+
+import Foundation
+extension Constant {
+    struct KeychainService {
+        static let account = "SysVPN"
+    }
+}
