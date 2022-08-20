d92fe1da33a29289fec5a0b24945947a5117994f
Nguyen Dinh Thach <nguyen.dinh.thach@linecorp.com>
2022-08-20 21:27:24 +0700

    Update new bundle id, fix get certificate api

---
+++ Shared/Networking/APIManager/ServiceManager+VPN.swift
@@ -0,0 +1,91 @@
+//
+//  ServiceManager+VPN.swift
+//  SysVPN
+//
+//  Created by Nguyen Dinh Thach on 19/08/2022.
+//
+
+import Foundation
+import RxSwift
+import Moya
+import SwiftyJSON
+import SwiftUI
+
+extension ServiceManager {
+    func getCountryList() -> Single<APIResponse<CountryListResultModel>> {
+        return request(.getCountryList)
+            .map { response in
+                let result = try JSONDecoder().decode(APIResponse<CountryListResultModel>.self, from: response.data)
+                return result
+            }
+            .catch { error in
+                throw APIError.someError
+            }
+    }
+    
+    func getRequestCertificate(currentTab: BoardViewModel.StateTab, asNewConnection: Bool = false) -> Single<APIResponse<RequestCerAPI>> {
+        return request(.getRequestCertificate(currentTab: currentTab, asNewConnection: asNewConnection))
+            .map { response in
+                let result = try JSONDecoder().decode(APIResponse<RequestCerAPI>.self, from: response.data)
+                return result
+            }
+            .catch { error in
+                throw APIError.someError
+            }
+    }
+    
+    func getObtainCertificate() -> Single<APIResponse<ObtainCertificateModel>> {
+        return request(.getObtainCertificate)
+            .map { response in
+                let result = try JSONDecoder().decode(APIResponse<ObtainCertificateModel>.self, from: response.data)
+                return result
+            }
+            .catch { error in
+                throw APIError.someError
+            }
+    }
+    
+    func getListSession(page: Int = 1, limit: Int = 20) -> Single<APIResponse<SessionResult>> {
+        return request(.getListSession(page: page, limit: limit))
+            .map { response in
+                let result = try JSONDecoder().decode(APIResponse<SessionResult>.self, from: response.data)
+                return result
+            }
+            .catch { error in
+                throw APIError.someError
+            }
+    }
+    
+    func disconnectSession(sessionId: String, terminal: Bool) -> Single<APIResponse<EmptyResult>> {
+        return request(.disconnectSession(sessionId: sessionId, terminal: terminal))
+            .map { response in
+                let result = try JSONDecoder().decode(APIResponse<EmptyResult>.self, from: response.data)
+                return result
+            }
+            .catch { error in
+                throw APIError.someError
+            }
+    }
+    
+    func getTopicQuestionList() -> Single<APIResponse<[TopicQuestionModel]>> {
+        return request(.getTopicQuestionList)
+            .map { response in
+                let result = try JSONDecoder().decode(APIResponse<[TopicQuestionModel]>.self, from: response.data)
+                return result
+            }
+            .catch { error in
+                throw APIError.someError
+            }
+    }
+    
+    func getMutihopList() -> Single<APIResponse<[MultihopModel]>> {
+        return request(.getMultihopList)
+            .map { response in
+                let result = try JSONDecoder().decode(APIResponse<[MultihopModel]>.self, from: response.data)
+                return result
+            }
+            .catch { error in
+                throw APIError.someError
+            }
+    }
+}
