fc85bd01f6c5f337090e9bf1c3824ffa0bbdc7ff
DinhILGMedia <dinh@sysvpn.com>
2022-09-07 20:15:38 +0700

    Fix slide

--- Shared/Screen/Introduction/Introduction.swift
+++ Shared/Screen/Introduction/Introduction.swift
@@ -8,32 +8,6 @@
 import Foundation
 import SwiftUI
 
-struct IntroductionPage: Hashable {
-    var id = UUID()
-    var image: Image
-    var title: String
-    var body: String
-    
-    static func getList() -> [IntroductionPage] {
-        
-        let intro1 = IntroductionPage(image: Asset.Assets.introduction1.swiftUIImage, title: L10n.Introduction.intro1Title, body: L10n.Introduction.intro1Body)
-        
-        let intro2 = IntroductionPage(image: Asset.Assets.introduction2.swiftUIImage, title: L10n.Introduction.intro2Title, body: L10n.Introduction.intro2Title)
-        
-        let intro3 = IntroductionPage(image: Asset.Assets.introduction3.swiftUIImage, title: L10n.Introduction.intro3Title, body: L10n.Introduction.intro3Body)
-        
-        return [intro1, intro2, intro3]
-    }
-    
-    static func == (lhs: IntroductionPage, rhs: IntroductionPage) -> Bool {
-        return lhs.id == rhs.id
-    }
-    
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(id)
-    }
-}
-
 struct IntroductionView: View {
     @State var index = 0
     @State private var signIn = false
@@ -51,26 +25,7 @@ struct IntroductionView: View {
                     Spacer()
                 }
                 Spacer()
-                PagingView(index: $index, maxIndex: IntroductionPage.getList().count - 1) {
-                    ForEach(IntroductionPage.getList(), id: \.self) { intro in
-                        VStack{
-                            intro.image
-                                .resizable()
-                                .scaledToFit()
-                            Spacer().frame(height: 10)
-                            Text(intro.title).setTitle()
-                                .multilineTextAlignment(.center)
-                            Spacer().frame(height: 10)
-                            Text(intro.body).setDefault()
-                                .multilineTextAlignment(.center)
-                        }
-                    }
-                }
-                .aspectRatio(3/4, contentMode: .fit)
-                .clipShape(RoundedRectangle(cornerRadius: 15))
-                .font(Font.body.monospacedDigit())
-                .frame(maxWidth: 400)
-                
+                SlideIntroduction()
                 AppButton(style: .themeButton, width: 300, height:50, text: L10n.Introduction.trialButton) {
                     self.signIn = true
                 }
---
+++ Shared/Screen/Introduction/SlideIntroduction.swift
@@ -0,0 +1,64 @@
+//
+//  SlideIntroduction.swift
+//  SysVPN
+//
+//  Created by Định Nguyễn on 07/09/2022.
+//
+
+import SwiftUI
+
+struct IntroductionPage: Hashable {
+    var id = UUID()
+    var image: Image
+    var title: String
+    var body: String
+    
+    static func getList() -> [IntroductionPage] {
+        
+        let intro1 = IntroductionPage(image: Asset.Assets.introduction1.swiftUIImage, title: L10n.Introduction.intro1Title, body: L10n.Introduction.intro1Body)
+        
+        let intro2 = IntroductionPage(image: Asset.Assets.introduction2.swiftUIImage, title: L10n.Introduction.intro2Title, body: L10n.Introduction.intro2Title)
+        
+        let intro3 = IntroductionPage(image: Asset.Assets.introduction3.swiftUIImage, title: L10n.Introduction.intro3Title, body: L10n.Introduction.intro3Body)
+        
+        return [intro1, intro2, intro3]
+    }
+    
+    static func == (lhs: IntroductionPage, rhs: IntroductionPage) -> Bool {
+        return lhs.id == rhs.id
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+}
+
+struct SlideIntroduction: View {
+    var body: some View {
+        TabView {
+            ForEach(IntroductionPage.getList(), id: \.self) { intro in
+                VStack{
+                    intro.image
+                        .resizable()
+                        .scaledToFit()
+                    Spacer().frame(height: 10)
+                    Text(intro.title).setTitle()
+                        .multilineTextAlignment(.center)
+                    Spacer().frame(height: 10)
+                    Text(intro.body).setDefault()
+                        .multilineTextAlignment(.center).padding(.bottom, 50)
+                }
+            }
+        }
+        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+        .aspectRatio(3/4,contentMode: .fit)
+        .frame(maxWidth:400)
+        .font(Font.body.monospacedDigit())
+    }
+}
+
+struct SlideIntroduction_Previews: PreviewProvider {
+    static var previews: some View {
+        SlideIntroduction()
+    }
+}
