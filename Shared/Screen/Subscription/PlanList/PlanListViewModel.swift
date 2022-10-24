33b4bf5c1c9e1e1ac6db773b7409a02700c8ac84
phanvandaILG <dapv@ilg-media.com>
2022-10-24 15:37:12 +0700

    check Payment

--- Shared/Screen/Subscription/PlanList/PlanListViewModel.swift
+++ Shared/Screen/Subscription/PlanList/PlanListViewModel.swift
@@ -20,7 +20,7 @@ struct Plan: Identifiable, Hashable {
     
     static let planA = Plan(subscriptionID: "sysvpn.ios.client.subscription.1year", name: L10n.PlanSelect.PlanA.title, description: L10n.PlanSelect.PlanA.description, price: L10n.PlanSelect.PlanA.price, savingText: L10n.PlanSelect.PlanA.savingText, note: L10n.PlanSelect.PlanA.note, noteTrial: L10n.PlanSelect.PlanA.noteTrial, get: L10n.PlanSelect.PlanA.get)
     
-    static let planB = Plan(subscriptionID: "sysvpn.ios.client.subscription.6months", name: L10n.PlanSelect.PlanB.title, description: L10n.PlanSelect.PlanB.description, price: L10n.PlanSelect.PlanB.price, savingText: L10n.PlanSelect.PlanB.savingText, note: L10n.PlanSelect.PlanB.note, noteTrial: L10n.PlanSelect.PlanB.noteTrial, get: L10n.PlanSelect.PlanB.get)
+    static let planB = Plan(subscriptionID: "sysvpn.ios.client.subscription.6month", name: L10n.PlanSelect.PlanB.title, description: L10n.PlanSelect.PlanB.description, price: L10n.PlanSelect.PlanB.price, savingText: L10n.PlanSelect.PlanB.savingText, note: L10n.PlanSelect.PlanB.note, noteTrial: L10n.PlanSelect.PlanB.noteTrial, get: L10n.PlanSelect.PlanB.get)
     
     static let planC = Plan(subscriptionID: "sysvpn.ios.client.subscription.1month", name: L10n.PlanSelect.PlanC.title, description: L10n.PlanSelect.PlanC.description, price: L10n.PlanSelect.PlanC.price, savingText: L10n.PlanSelect.PlanC.savingText, note: L10n.PlanSelect.PlanC.note, noteTrial: L10n.PlanSelect.PlanC.noteTrial, get: L10n.PlanSelect.PlanC.get)
     
